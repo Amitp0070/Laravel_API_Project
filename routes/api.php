f309aeef4477c75c1a554b58626c8b818b2cf630
AmitPathak <amit7081483564@gmail.com>
2025-03-25 17:04:50 +0530

    Refactor API routes to group post management under authentication middleware

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,22 @@ use Illuminate\Support\Facades\Route;
 Route::post('signup', [AuthController::class, 'signup']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/posts', [PostController::class, 'index']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-Route::middleware('auth:sanctum')->post('/create-post', [PostController::class, 'store']);
-Route::middleware('auth:sanctum')->get('/post/{id}', [PostController::class, 'show']);
-Route::middleware('auth:sanctum')->post('/update-post/{id}', [PostController::class, 'update']);
-Route::middleware('auth:sanctum')->delete('/delete-post/{id}', [PostController::class, 'destroy']);
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth Routes
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Post Routes
+    Route::get('/posts', [PostController::class, 'index']);
+    Route::post('/create-post', [PostController::class, 'store']);
+    Route::get('/post/{id}', [PostController::class, 'show']);
+    Route::post('/update-post/{id}', [PostController::class, 'update']);
+    Route::delete('/delete-post/{id}', [PostController::class, 'destroy']);
+});
+
+
+// Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+// Route::middleware('auth:sanctum')->get('/posts', [PostController::class, 'index']);
+// Route::middleware('auth:sanctum')->post('/create-post', [PostController::class, 'store']);
+// Route::middleware('auth:sanctum')->get('/post/{id}', [PostController::class, 'show']);
+// Route::middleware('auth:sanctum')->post('/update-post/{id}', [PostController::class, 'update']);
+// Route::middleware('auth:sanctum')->delete('/delete-post/{id}', [PostController::class, 'destroy']);
